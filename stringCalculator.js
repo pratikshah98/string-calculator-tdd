988ee8a9d54484e2e60ac2367c6702569c22240f
Pratik Shah <pratik@selcom.net>
2024-11-29 00:13:29 +0530

    first commit implementation of tdd

---
+++ stringCalculator.js
@@ -0,0 +1,8 @@
+// stringCalculator.js
+function add(numbers) {
+    if (numbers === "") return 0;
+    return 0;
+  }
+  
+  module.exports = { add };
+  
---
+++ stringCalculator.test.js
@@ -0,0 +1,6 @@
+// stringCalculator.test.js
+const { add } = require('./stringCalculator');
+
+test('returns 0 for an empty string', () => {
+  expect(add("")).toBe(0);
+});
