f1c62ece5030b813d37c7d2290e317aecb533d13
Pratik Shah <pratik@selcom.net>
2024-11-29 00:33:24 +0530

    4_step_excute

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,16 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    const numArray = numbers.split(/[\n,]/).map(Number);
+  
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
+      delimiter = new RegExp(delimiterMatch[1]);
+      numbers = numbers.split("\n").slice(1).join("\n");
+    }
+  
+    const numArray = numbers.split(delimiter).map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
   }
+  
   
   module.exports = { add };  
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,7 +1,7 @@
 const { add } = require('./stringCalculator');
 
-test('handles newlines as delimiters', () => {
-    expect(add("1\n2,3")).toBe(6);
+test('supports custom delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
   });
   
   
