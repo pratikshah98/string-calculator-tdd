74d974f936aab569d1b806be7dca721e2ad88b19
Pratik Shah <pratik@selcom.net>
2024-11-29 00:31:55 +0530

    2.4_step_excute

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,6 +1,7 @@
 const { add } = require('./stringCalculator');
 
-test('returns the sum of two numbers', () => {
-    expect(add("1,5")).toBe(6);
+test('returns the sum of multiple numbers', () => {
+    expect(add("1,2,3,4,5")).toBe(15);
   });
+  
   
